bc6cf3169a28be2ac3d857ea9bf28aa2477721b7
GuanAllen <39507681+GuanAllen@users.noreply.github.com>
2026-09-24 11:59:51 +0800

    publish plugins.v3/magicflow/src/components/Config.vue

---
+++ plugins.v3/magicflow/src/components/Config.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted } from 'vue'
+import MagicFlowWorkbench from './MagicFlowWorkbench.vue'
+
+defineProps({
+  initialConfig: { type: Object, default: () => ({}) },
+  api: { type: Object, default: () => ({}) },
+})
+
+const emit = defineEmits(['layout', 'close'])
+
+onMounted(() => {
+  emit('layout', { maxWidth: '80rem' })
+})
+</script>
+
+<template>
+  <MagicFlowWorkbench
+    :api="api"
+    plugin-id="MagicFlow"
+    initial-tab="config"
+    show-close
+    compact
+    @close="$emit('close')"
+  />
+</template>
