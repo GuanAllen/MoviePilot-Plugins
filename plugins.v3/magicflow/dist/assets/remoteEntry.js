2443a5fa36aaca4097912d7a704a5ce5d2962f08
GuanAllen <39507681+GuanAllen@users.noreply.github.com>
2026-09-24 20:12:18 +0800

    publish plugins.v3/magicflow/dist/assets/remoteEntry.js

--- plugins.v3/magicflow/dist/assets/remoteEntry.js
+++ plugins.v3/magicflow/dist/assets/remoteEntry.js
@@ -2,17 +2,17 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./Page":()=>{
-      dynamicLoadingCss(["MagicFlowWorkbench-ZUf0yQaZ.css"], false, './Page');
-      return __federation_import('./__federation_expose_Page-Cn49B8KK.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
+      dynamicLoadingCss(["MagicFlowWorkbench-D2yIa9bd.css"], false, './Page');
+      return __federation_import('./__federation_expose_Page-0osydpOZ.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
 "./Config":()=>{
-      dynamicLoadingCss(["MagicFlowWorkbench-ZUf0yQaZ.css"], false, './Config');
-      return __federation_import('./__federation_expose_Config-BQ8MDF1Y.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
+      dynamicLoadingCss(["MagicFlowWorkbench-D2yIa9bd.css"], false, './Config');
+      return __federation_import('./__federation_expose_Config-CE2U_mdO.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
 "./Dashboard":()=>{
       dynamicLoadingCss(["__federation_expose_Dashboard-6fKvXuF3.css"], false, './Dashboard');
       return __federation_import('./__federation_expose_Dashboard-Bm62vti0.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
 "./AppPage":()=>{
-      dynamicLoadingCss(["MagicFlowWorkbench-ZUf0yQaZ.css"], false, './AppPage');
-      return __federation_import('./__federation_expose_AppPage-B10Ufb6N.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["MagicFlowWorkbench-D2yIa9bd.css"], false, './AppPage');
+      return __federation_import('./__federation_expose_AppPage-BGNW083B.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
