4e31abb84add58809985d62ffb79baba06cebb55
GuanAllen <39507681+GuanAllen@users.noreply.github.com>
2026-09-24 11:59:59 +0800

    publish plugins.v3/magicflow/src/components/Page.vue

---
+++ plugins.v3/magicflow/src/components/Page.vue
@@ -0,0 +1,20 @@
+<script setup>
+import MagicFlowWorkbench from './MagicFlowWorkbench.vue'
+
+defineProps({
+  api: { type: Object, default: () => ({}) },
+})
+
+defineEmits(['action', 'close'])
+</script>
+
+<template>
+  <MagicFlowWorkbench
+    :api="api"
+    plugin-id="MagicFlow"
+    show-close
+    compact
+    @action="$emit('action')"
+    @close="$emit('close')"
+  />
+</template>
