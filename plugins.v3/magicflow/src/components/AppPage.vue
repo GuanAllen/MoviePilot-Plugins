2201f2caf061f18cef6e07b0ac00d9f42c1f4882
GuanAllen <39507681+GuanAllen@users.noreply.github.com>
2026-09-24 11:59:49 +0800

    publish plugins.v3/magicflow/src/components/AppPage.vue

---
+++ plugins.v3/magicflow/src/components/AppPage.vue
@@ -0,0 +1,19 @@
+<script setup>
+import MagicFlowWorkbench from './MagicFlowWorkbench.vue'
+
+defineProps({
+  api: { type: Object, default: () => ({}) },
+  navKey: { type: String, default: 'main' },
+  pluginId: { type: String, default: 'MagicFlow' },
+})
+
+defineEmits(['action'])
+</script>
+
+<template>
+  <MagicFlowWorkbench
+    :api="api"
+    :plugin-id="pluginId"
+    @action="$emit('action')"
+  />
+</template>
